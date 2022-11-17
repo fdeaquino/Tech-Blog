375d7da3749f1d587dd602c719890b44ab06e468
FDeaquino <fidel.deaquino@gmail.com>
2022-11-17 14:08:41 -0600

    installed handlebars, created handlebars templates, set up the MVC folder structure, created route for homepage, added css from the previous project and modified it a bit

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// express handlebars
+const hbs = exphbs.create({});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// Express.js middleware that takes the contents of this folder and serves them as static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+// express handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
